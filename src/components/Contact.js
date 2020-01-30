ee6b8fff5a011f8917891de089add3ff852b5e6d
Andrea de Roeck <deroeck.andrea@gmail.com>
2020-01-30 12:55:07 +0000

    Create contact list and change app import

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Contact.css';
 
 
+
 function Contact(props) {
   return (
     <div className="Contact">
-      <img src={props.avatar} className="avatar" />
+      <img src={props.avatar} alt={props.name} className="avatar"/>
       <div>
         <h4 className="name">{props.name}</h4>
         <div className="status">
---
+++ src/components/ContactList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Contact from './Contact';
+
+//5 data users
+
+const contacts = [
+    {
+        id: 0,
+        avatar: "https://randomuser.me/api/portraits/women/64.jpg",
+        name: "Ross Shaw",
+        online: true
+    },
+    {
+        id: 1,
+        avatar: "https://randomuser.me/api/portraits/men/92.jpg",
+        name: "Howard Mason",
+        online: false
+    },
+    {
+        id: 2,
+        avatar: "https://randomuser.me/api/portraits/women/56.jpg",
+        name: "Lorraine Craig",
+        online: true
+    },
+    {   
+        id: 3,
+        avatar: "https://randomuser.me/api/portraits/men/23.jpg",
+        name: "Andre Nelson",
+        online: true
+    },
+      {
+        id: 4,  
+        avatar: "https://randomuser.me/api/portraits/men/15.jpg",
+        name: "Lonnie Hunter",
+        online: false
+    }
+];
+
+const ContactList = () => (
+    <React.Fragment>
+      {contacts.map(item => (
+        <Contact key ={item.id} avatar={item.avatar} name={item.name} online={item.online} />
+        ))}
+    </React.Fragment>
+  );
+
+
+
+export default ContactList;
